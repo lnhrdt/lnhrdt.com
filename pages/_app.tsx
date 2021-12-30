1a468772f20a99bd88423aba9f95219e8b0039a0
Leonhardt Koepsell <hello@lnhrdt.com>
2021-12-30 00:06:28 -0800

    Create a simple contact page based on MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import {createTheme, CssBaseline, ThemeProvider} from '@mui/material'
+import type {AppProps} from 'next/app'
+import {teal} from '@mui/material/colors'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const theme = createTheme({
+    palette: {background: {default: teal[200]}},
+})
 
-export default MyApp
+const App = ({Component, pageProps}: AppProps) => (
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Head>
+            <title>Leonhardt Koepsell</title>
+            <meta name="description" content="Homepage for Leonhardt Koepsell"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <Component {...pageProps} />
+    </ThemeProvider>
+)
+
+export default App
